18fe4eb251b46d42eea61bfe45bf55298f89a00b
Ashish Dwivedi <dwivediashish.work@gmail.com>
2023-07-07 15:17:49 +0530

    Initial Commit

---
+++ BlasterTournament/BlasterTournamentApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlasterTournamentApp.swift
+//  BlasterTournament
+//
+//  Created by Ashish Dwivedi on 07/07/23.
+//
+
+import SwiftUI
+
+@main
+struct BlasterTournamentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
