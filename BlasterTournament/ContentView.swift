a456b923fb45eeca318cbc3e5ed57b4a6f5dc869
Ashish Dwivedi <dwivediashish.work@gmail.com>
2023-07-07 15:34:23 +0530

    Create list with player as a rough mockup.

--- BlasterTournament/ContentView.swift
+++ BlasterTournament/ContentView.swift
@@ -7,20 +7,51 @@
 
 import SwiftUI
 
+typealias Players = [Player]
+struct Player: Identifiable {
+    var id: Int
+    var imageName: String
+    var name: String
+    var score: Int
+}
+
 struct ContentView: View {
+    private static let title: String = "Star Wars Blaster Tournament"
+    private static let headerTitle: String = "Points Table"
+    
+    var players: [Player] = Player.dummy
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack(alignment: .leading) {
+                Divider()
+                Text(Self.headerTitle)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 10)
+                List(players) { player in
+                    Text(player.name)
+                }
+                .listStyle(.plain)
+                .navigationTitle(Self.title)
+                .navigationBarTitleDisplayMode(.inline)
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension Player {
+    static var dummy: [Player] {
+        [
+            Player(id: 0, imageName: "a-image", name: "Darth Vader", score: 22),
+            Player(id: 1, imageName: "a-image", name: "Darth Vader1", score: 222),
+            Player(id: 3, imageName: "a-image", name: "Darth Vader2", score: 2),
+            Player(id: 3, imageName: "a-image", name: "Darth Vader3", score: 0)
+        ]
     }
 }
